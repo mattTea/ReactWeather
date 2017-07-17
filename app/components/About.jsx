d88b16e4205d25bd90a33ad63fea8f4fa1e8a9da
Matt Thompson <matt34thompson@gmail.com>
2017-07-17 21:57:11 +0100

    Style About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,23 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">Welcome to the React Weather App.</p>
+      <p className="text-center">Go ahead, search for the current temperature<br></br><em>anywhere</em> in the world!</p>
+      <p className="text-center"><small>Built in React for the Complete React Web Developer Course.</small></p>
+      <p>Here are some of the tools and resources used...</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Used to search for weather data by city.
+        </li>
+      </ul>
+      <blockquote className="text-center">
+        The React Weather App is so quick and slick
+        <cite>Matt Tea</cite>
+      </blockquote>
     </div>
   )
 };
